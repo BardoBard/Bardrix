3e53da688a48f4916300f16110229c0c1f6dfd92
Bard <nigel.terpstra@student.nhlstenden.com>
2024-03-04 03:12:04 +0100

    CMake files added to project

---
+++ Bardrix/include/bardrix/bardrix.h
@@ -0,0 +1,10 @@
+//
+// Created by Bard on 29/02/2024.
+//
+
+#pragma once
+
+class bardrix {
+public:
+    static void print();
+};
---
+++ Bardrix/src/bardrix/bardrix.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Bard on 29/02/2024.
+//
+#include <bardrix/bardrix.h>
+#include <iostream>
+
+void bardrix::print() {
+    std::cout << "Hello, World!" << std::endl;
+}
