4ecdb29bf3bdbf8d82e4dba638562c8be8293d9f
Bardio <nigel.terpstra@nhlstenden.com>
2024-05-10 02:18:23 +0200

    Added simple material + shape + tests, moved a line in light

--- Bardrix/src/bardrix/light.cpp
+++ Bardrix/src/bardrix/light.cpp
@@ -4,18 +4,16 @@
 
 #include <bardrix/light.h>
 
-#include <utility>
-
 namespace bardrix {
-
-    double light::inverse_square_law_squared(const double distance_squared) const noexcept {
-        if (nearly_equal(distance_squared, 0)) return HUGE_VAL;
-        return intensity_ / distance_squared;
-    }
 
     light::light(point3 position, double intensity, const bardrix::color& color) noexcept
             : position{ std::move(position) }, color{ color } {
         set_intensity(intensity);
+    }
+
+    double light::inverse_square_law_squared(const double distance_squared) const noexcept {
+        if (nearly_equal(distance_squared, 0)) return HUGE_VAL;
+        return intensity_ / distance_squared;
     }
 
     double light::inverse_square_law(const point3& point) const noexcept {
